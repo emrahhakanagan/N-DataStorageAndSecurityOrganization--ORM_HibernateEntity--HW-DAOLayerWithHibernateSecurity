9cd823107213d51a491c106bf78f122fcd62821d
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-05-22 22:33:30 +0300

    entity added

---
+++ src/main/java/com/agan/layerdao_hibernate/entity/Person.java
@@ -0,0 +1,42 @@
+package com.agan.layerdao_hibernate.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(schema = "hibernate_hw", name = "person")
+@IdClass(PersonId.class)
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Id
+    @Column(name = "name")
+    private String name;
+
+    @Id
+    @Column(name = "surname")
+    private String surname;
+
+    @Id
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "city_of_living")
+    private String cityOfLiving;
+
+
+}
+
---
+++ src/main/java/com/agan/layerdao_hibernate/entity/PersonId.java
@@ -0,0 +1,18 @@
+package com.agan.layerdao_hibernate.entity;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+
+public class PersonId implements Serializable {
+
+    private long id;
+    private String name;
+    private String surname;
+    private int age;
+
+
+}
+
