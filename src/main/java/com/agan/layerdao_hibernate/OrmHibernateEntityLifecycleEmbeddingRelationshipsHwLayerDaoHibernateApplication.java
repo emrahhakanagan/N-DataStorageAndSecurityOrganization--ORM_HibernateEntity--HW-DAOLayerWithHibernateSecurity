ac16c91f95ebf50b11ac29a8544a7cdfc3f4b0b8
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-05-22 20:57:04 +0300

    basic configurations have done

---
+++ src/main/java/com/agan/layerdao_hibernate/OrmHibernateEntityLifecycleEmbeddingRelationshipsHwLayerDaoHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.agan.layerdao_hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmHibernateEntityLifecycleEmbeddingRelationshipsHwLayerDaoHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmHibernateEntityLifecycleEmbeddingRelationshipsHwLayerDaoHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agan/layerdao_hibernate/OrmHibernateEntityLifecycleEmbeddingRelationshipsHwLayerDaoHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agan.layerdao_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrmHibernateEntityLifecycleEmbeddingRelationshipsHwLayerDaoHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
