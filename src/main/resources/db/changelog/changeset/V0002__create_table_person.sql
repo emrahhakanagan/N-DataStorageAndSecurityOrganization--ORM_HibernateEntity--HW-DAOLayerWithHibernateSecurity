ab50a4bcc46d2b0afa64ed53331d9af08613fc87
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-05-26 00:46:56 +0300

    liquibase files and app-prop added and uptaded

---
+++ src/main/resources/db/changelog/changeset/V0002__create_table_person.sql
@@ -0,0 +1,9 @@
+create table orm_hibernate_hw.person (
+id BIGSERIAL NOT NULL,
+name VARCHAR(255) NOT NULL,
+surname VARCHAR(255) NOT NULL,
+age INT NOT NULL,
+phone_number VARCHAR(20) NOT NULL,
+city_of_living VARCHAR(100),
+CONSTRAINT person_pkey PRIMARY KEY (name, surname, age)
+);
