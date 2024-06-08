161ecb15b3fc26af64c476673802becdd097d321
Emrah Hakan AGAN <134237010+emrahhakanagan@users.noreply.github.com>
2024-06-08 16:35:14 +0300

    migration create table person uptaded

--- src/main/resources/db/changelog/changeset/V0002__create_table_person.sql
+++ src/main/resources/db/changelog/changeset/V0002__create_table_person.sql
@@ -1,8 +1,7 @@
 create table orm_hibernate_hw.person (
-id BIGSERIAL,
-name VARCHAR(255) NOT NULL,
-surname VARCHAR(255) NOT NULL,
-age INT NOT NULL,
+name VARCHAR(255) NOT NULL UNIQUE ,
+surname VARCHAR(255) NOT NULL UNIQUE ,
+age INT NOT NULL UNIQUE ,
 phone_number VARCHAR(20) NOT NULL,
 city_of_living VARCHAR(100),
 CONSTRAINT person_pkey PRIMARY KEY (name, surname, age)
